27546aea7f62afd213e2e2bbd8e22effad60f3ae
Julius <76077674+juliushuerta@users.noreply.github.com>
2024-11-30 22:09:59 +0800

    init push
    init push for my personal portfolio

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  
